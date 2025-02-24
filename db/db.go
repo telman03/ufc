e8d55f13437d0b14772df912b45f7875a9342de2
telman03 <telmangadimov1@gmail.com>
2025-02-24 22:04:23 +0400

    Configured for Railway deployment

--- db/db.go
+++ db/db.go
@@ -1,11 +1,10 @@
 package db
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,23 +13,12 @@ var DB *gorm.DB
 
 
 func ConnectDB() {
-	err := godotenv.Load()
-	if err!=nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"),
-	)
-
+	dsn := os.Getenv("DATABASE_URL") // Use Railway DB URL
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil{
-		log.Fatal("Failed to connect to database: ", err)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
 	}
-	DB = db
 
-	fmt.Println("Database connected succesfully")
-	
+	DB = db
+	log.Println("Database connected successfully!")
 }
